47abf50fd740c244e2661ef47873ea0043707dac
Jafeth <132289436+JafethHPC@users.noreply.github.com>
2025-11-25 11:09:02 -0500

    LON-11 Added CORS configuration for endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import weaviate
 import weaviate.classes.query as wvq
@@ -6,6 +7,18 @@ from app.core.config import settings
 from langsmith import Client
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class SearchRequest(BaseModel):
     query: str
